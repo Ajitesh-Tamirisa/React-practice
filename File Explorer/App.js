083d27c0f0ed39cccc1dcd5d0928955e9181f3e4
Sathyasai Ajitesh Tamirisa <60847274+Ajitesh-Tamirisa@users.noreply.github.com>
2025-05-22 21:08:43 -0700

    Update App.js

--- File Explorer/App.js
+++ File Explorer/App.js
@@ -1,3 +1,27 @@
+// https://www.greatfrontend.com/questions/user-interface/file-explorer?practice=practice&tab=coding
+
+// File Explorer
+
+// Given an array of file objects, build a component that displays them in a hierarchical tree format.
+
+// There are two types of objects – files and directories:
+
+// Files are essentially leaf nodes of the tree, they do not have children.
+// Directories can contain other objects – either files or directories.
+// You may assume that the IDs and names within the same directory are unique.
+
+// Requirements
+// The names of the directories/files should be displayed.
+// Directories
+// Contents of the directory should be displayed in a manner that indicates it belongs within the directory. The recommended approach is to indent the contents to the right.
+// Directories can be expanded and collapsed. Clicking on a directory should toggle its expanded/collapsed state.
+// Directories should appear before files. All the items should be sorted alphabetically within their respective categories.
+// You may style the expand/collapse functionality in a way you prefer as long as it is clear that the item is a directory and whether it is in the expanded or collapsed state.
+// Directories can be empty.
+// Files
+// Files are not expandable and cannot be interacted with.
+// The focus of the exercise is on the functionality and not the styling.
+
 import "./styles.css";
 import React, { useEffect, useState } from "react";
 import data from "./data.json";
